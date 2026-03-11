063b407f032f1a484d957b21dd304f686f94fc3c
Alif Rachmawadi <dev@subosito.com>
2026-03-11 13:04:32 +0000

    reformat

--- flake.nix
+++ flake.nix
@@ -6,21 +6,19 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
 
-      perSystem = { pkgs, system, ... }:
-        let
-          kotlin-lsp = import ./packages/kotlin-lsp { inherit pkgs; };
-        in
-        {
-          devShells.default = import ./shell.nix { inherit pkgs; };
-
-          packages = {
-            kotlin-lsp = kotlin-lsp;
-          };
-        };
+      perSystem = {pkgs, ...}: {
+        devShells.default = import ./shell.nix {inherit pkgs;};
+        packages.kotlin-lsp = import ./packages/kotlin-lsp {inherit pkgs;};
+      };
 
       flake = {
         overlays.default = final: _: {
